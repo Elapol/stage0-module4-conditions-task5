dfe6a5a7dae027d4ae81abaa15ea4528d9327040
elyad <ella.dikovich@gmail.com>
2022-11-01 22:30:12 +0100

    era

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void main(String[] args) {
+        isLeapYear(2012);
+    }
+    public static void isLeapYear(int year) {
+        if (year/400 !=0 && year%400 ==0 || year/4 !=0 && year%4 ==0){
+                System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
+
+
     }
 }
